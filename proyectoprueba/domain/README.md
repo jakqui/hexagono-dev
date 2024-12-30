68e4638ce9041dfff92388f032ba4dbd4ac93423
Jakqueline Herrera Garcia <38226697+jakqui@users.noreply.github.com>
2024-12-30 20:54:16 +0000

    Primeros cambios

---
+++ proyectoprueba/domain/README.md
@@ -0,0 +1,3 @@
+# Project mx.hexagonodev/domain
+
+Build the project and run all tests with `./mvnw package` or `mvnw.cmd package` for Windows.
---
+++ proyectoprueba/domain/src/main/java/mx/hexagonodev/proyectoprueba/domain/Main.java
@@ -0,0 +1,8 @@
+package mx.hexagonodev.proyectoprueba.domain;
+
+public class Main {
+
+  public String format(String input) {
+    return input.toUpperCase();
+  }
+}
